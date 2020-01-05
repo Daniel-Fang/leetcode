74ff609137678ca32658749694f1ad14d3aa1e27
Daniel-Fang <f455039690@163.com>
2020-01-05 23:33:27 +0800

    ...

---
+++ 最接近的三数之和.js
@@ -0,0 +1,12 @@
+// leetcode 16 description
+// 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+// TAG: 数组 双指针
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    
+};
